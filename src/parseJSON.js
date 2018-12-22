92a239511142dd59e45b78df2bcedc6b43a7b2f6
rdkerr <rogerdkerr@gmail.com>
2018-12-21 19:43:50 -0700

    First attempt at array

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -5,12 +5,14 @@
 var parseJSON = function(json) {
   // your code goes here  
   console.log(json);
+  json = json.trim();
   if (isArray(json)) {
     let result = [];
     if (json.length === 2) {
       return result;
     }
-
+    let elements = json.slice(1, json.length - 1).split(',');
+    result = elements.map(parseJSON);
     return result;
   } else if (isObj(json)) {
     let result = {};
@@ -19,7 +21,11 @@ var parseJSON = function(json) {
     }
     let elements = json.slice(1, json.length - 1).split(',');
     for (let i = 0; i < elements.length; i ++) {
-      let keyValue = elements[i].split(': ');
+      let keyValue = elements[i].split(':');
+      result[parseJSON(keyValue[0])] = parseJSON(keyValue[1]);
+    }
+    for (let i = 0; i < elements.length; i ++) {
+      let keyValue = elements[i].split(':');
       result[parseJSON(keyValue[0])] = parseJSON(keyValue[1]);
     }
     return result;
@@ -27,13 +33,13 @@ var parseJSON = function(json) {
     return +json;
   } else if (isString(json)) {
     return json.slice(1, json.length - 1);
-  } else if (json === 'null') { 
+  } else if (json === null) { 
     return null;
-  } else if (json === 'false') { 
+  } else if (json === false) { 
     return false;
-  } else if (json === 'true') { 
+  } else if (json === true) { 
     return true;
-  } else if (json === 'undefined') { 
+  } else if (json === undefined) { 
     return undefined;
   }
   throw new SyntaxError('Unexpected end of input');
@@ -55,3 +61,4 @@ var isArray = function(array) {
 var isObj = function(obj) {
   return obj[0] === '{' && obj[obj.length - 1] === '}';
 };
+
